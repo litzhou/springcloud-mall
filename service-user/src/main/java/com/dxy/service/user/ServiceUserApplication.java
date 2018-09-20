7b0f3a374823b8be14f824ce1372bc5fe99a1a03
zhougaojun <zhougaojun@dxy.cn>
2018-09-20 18:01:14 +0800

    用户服务更新

--- service-user/src/main/java/com/dxy/service/user/ServiceUserApplication.java
+++ service-user/src/main/java/com/dxy/service/user/ServiceUserApplication.java
@@ -1,7 +1,8 @@
-package com.dxy.api.gateway;
+package com.dxy.service.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceUserApplication {
 
     public static void main(String[] args) {
