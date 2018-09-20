5d38e42d6fdaa1afabc4f29b628e5ed2f7f55d5d
zhougaojun <zhougaojun@dxy.cn>
2018-09-20 15:47:05 +0800

    服务网关

---
+++ api-gateway/src/main/java/com/dxy/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.dxy.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * user service
+ * @author zhougaojun
+ * @since 2018-08-30
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
